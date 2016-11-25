0cadfba30dcbbf649646e9c191c51e38754821a9
liu1107303121 <tj@TJdeMacBook-Pro.local>
2016-11-25 11:07:49 +0800

    first commit

---
+++ AutoBuild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoBuild
+//
+//  Created by TJ on 2016/11/25.
+//  Copyright © 2016年 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoBuild/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoBuild
+//
+//  Created by TJ on 2016/11/25.
+//  Copyright © 2016年 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
